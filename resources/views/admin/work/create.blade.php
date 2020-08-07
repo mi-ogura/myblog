93cfe27493dd1f2638afc41072d9f6f51ab5996a
mi-ogura <m-ogura@cmind-co.jp>
2020-08-08 07:35:29 +0900

    3-7復習　課題

--- resources/views/admin/work/create.blade.php
+++ resources/views/admin/work/create.blade.php
@@ -1,13 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>work</title>
-    </head>
-    <body>
-        <h1>work.create</h1>
-    </body>
-</html>
+@extends('layouts.work')
+
+
+@section('title', '職務経歴作成')
+
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>職務経歴作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
