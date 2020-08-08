5b3effa441ce70934a23e5e2da6eca729df7c105
mi-ogura <m-ogura@cmind-co.jp>
2020-08-08 14:49:16 +0900

    3-8復習　課題

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
+Route::group(['prefix' => 'admin','middleware'=>'auth'], function() {
      Route::get('work/edit', 'Admin\WorkController@edit');
      Route::get('work/create', 'Admin\WorkController@add');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
