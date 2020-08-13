a2339cebab51b226ebe7ee1b3c8997187304afd3
mi-ogura <m-ogura@cmind-co.jp>
2020-08-13 15:32:44 +0900

    3-10復習　課題

--- app/Http/Controllers/Admin/WorkController.php
+++ app/Http/Controllers/Admin/WorkController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Work;
 
 class WorkController extends Controller
 {
@@ -14,6 +15,28 @@ class WorkController extends Controller
 
 public function create(Request $request)
 {
+    $this->validate($request, Work::$rules);
+
+      $work = new Work;
+      $form = $request->all();
+
+      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+      if (isset($form['image'])) {
+        $path = $request->file('file')->store('public/image');
+        $news->file_path = basename($path);
+      } else {
+          $news->file_path = null;
+      }
+
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['file']);
+
+      // データベースに保存する
+      $news->fill($form);
+      $news->save();
+
     return redirect('admin/work/create');
 }
 
